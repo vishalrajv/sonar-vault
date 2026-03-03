a963fb7afd1bc9f7c8e5badcc2ca537a67a0cfad
Vishal Raj V <vishal.218023@gmail.com>
2026-03-03 14:12:23 +0530

    feat(auth): Add get_current_active_user dependency

--- api/v1/auth.py
+++ api/v1/auth.py
@@ -44,6 +44,11 @@ async def get_current_user(db: Session = Depends(get_db), token: str = Depends(o
         
     return user
 
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
 @router.post("/login", response_model=Token)
 def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == user_credentials.username).first()
